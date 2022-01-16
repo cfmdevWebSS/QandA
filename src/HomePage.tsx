f994c81de465e41c08c5e3dbf69c2a5966bbd4cb
Lawrence Buta <larry.buta@gmail.com>
2022-01-15 21:34:34 -0500

    Interim Save-Chapter3-ASP.NET5-01-15-2022

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { QuestionList } from './QuestionList';
 import { getUnansweredQuestions } from './QuestionData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
 
 export const HomePage = () => (
-  <div>
+  <Page>
     <div>
-      <h2>Unanswered Questions</h2>
-      <button>Ask a Question</button>
+      <PageTitle>Unanswered Questions</PageTitle>
+      <div>
+        <button>Ask a Question</button>
+      </div>
+      <QuestionList data={getUnansweredQuestions()} />
     </div>
-    <QuestionList data={getUnansweredQuestions()} />
-  </div>
+  </Page>
 );
