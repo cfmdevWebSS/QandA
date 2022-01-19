d0fcdc56b182d9bf0df96ecbaf5713a33be930eb
Lawrence Buta <larry.buta@gmail.com>
2022-01-18 19:48:01 -0500

    Interim Save-Chapter3-ASP.NET5-01-18-2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import { css } from '@emotion/react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
-import './App.css';
+import { fontFamily, fontSize, gray2 } from './Styles';
 
 function App() {
   return (
-    <div className="App">
+    <div
+      css={css`
+        font-family: ${fontFamily};
+        font-size: ${fontSize};
+        color: ${gray2};
+      `}
+    >
       <Header />
       <HomePage />
     </div>
--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,12 +1,15 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import { css } from '@emotion/react';
 import React from 'react';
 import { QuestionList } from './QuestionList';
 import { getUnansweredQuestions, QuestionData } from './QuestionData';
 import { Page } from './Page';
 import { PageTitle } from './PageTitle';
+import { PrimaryButton } from './Styles';
 
 export const HomePage = () => {
   const [questions, setQuestions] = React.useState<QuestionData[]>([]);
-
   const [questionsLoading, setQuestionsLoading] = React.useState(true);
 
   React.useEffect(() => {
@@ -17,24 +20,29 @@ export const HomePage = () => {
     };
     doGetUnansweredQuestions();
   }, []);
-
   const handleAskQuestionClick = () => {
-    console.log('TODO: move to the Ask Page');
+    console.log('TODO - move to the AskPage');
   };
 
   return (
     <Page>
-      <div>
+      <div
+        css={css`
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        `}
+      >
         <PageTitle>Unanswered Questions</PageTitle>
-        <div>
-          <button onClick={handleAskQuestionClick}>Ask a Question</button>
-        </div>
-        {questionsLoading ? (
-          <div>Loading...</div>
-        ) : (
-          <QuestionList data={questions || []} />
-        )}
+        <PrimaryButton onClick={handleAskQuestionClick}>
+          Ask a question
+        </PrimaryButton>
       </div>
+      {questionsLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <QuestionList data={questions} />
+      )}
     </Page>
   );
 };
