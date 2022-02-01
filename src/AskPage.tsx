6f5a6bfd8287c02d259c63cc2bf4f772be66efa7
Lawrence Buta <larry.buta@gmail.com>
2022-02-01 18:43:14 -0500

    Interim Save-Chapter6-ASP.NET5-02-01-2022

--- src/AskPage.tsx
+++ src/AskPage.tsx
@@ -1,4 +1,44 @@
 import React from 'react';
 import { Page } from './Page';
-
-export const AskPage = () => <Page title="Ask a Question">{null}</Page>;
+import {
+  Fieldset,
+  FieldContainer,
+  FieldLabel,
+  FieldInput,
+  FieldTextArea,
+  FormButtonContainer,
+  PrimaryButton,
+} from './Styles';
+import { useForm } from 'react-hook-form';
+type FormData = { title: string; content: string };
+export const AskPage = () => {
+  const { register } = useForm<FormData>();
+  return (
+    <Page title="Ask a Question">
+      <form>
+        <Fieldset>
+          <FieldContainer>
+            <FieldLabel htmlFor="title">Title</FieldLabel>
+            <FieldInput
+              {...register('title')}
+              id="title"
+              name="title"
+              type="text"
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <FieldLabel htmlFor="content">Content</FieldLabel>
+            <FieldTextArea
+              {...register('content')}
+              id="content"
+              name="content"
+            />
+          </FieldContainer>
+          <FormButtonContainer>
+            <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
+          </FormButtonContainer>
+        </Fieldset>
+      </form>
+    </Page>
+  );
+};
